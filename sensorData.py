9c1243b283c949c94a3ed509c7b00a63bced7068
amarchheda <amar.chheda@hypergiant.com>
2021-05-07 20:31:18 -0500

    starter code

---
+++ sensorData.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def index():
+    data = request.get_json()
+    print(data)
+    return json.dumps({'success':True}), 200, {'contentType':'application/json'}
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=80, host="0.0.0.0")
